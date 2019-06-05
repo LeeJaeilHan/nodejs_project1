b25cd6fc9e642fc49a23cb3c31f4229d59f66d83
LeeJaeilHan <wodlf1212@hanyang.ac.kr>
2019-06-05 17:48:50 +0900

    hello server2

--- main.js
+++ main.js
@@ -2,8 +2,28 @@ const http = require('http');
 const fs = require('fs');
 
 var app = http.createServer(function(request,response){
+  const url = request.url;
+  let template = '';
+  if(url=='/') {
+    template =
+    `
+    <!doctype html>
+    <html>
+      <head>
+        <title>hello Server</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h2>Hello server</h2>
+        <p>
+          Let's start server
+        </p>
+      </body>
+    </html>
+    `
+  }
   response.writeHead(200);
-  response.end("hello server");
+  response.end(template);
 });
 
 app.listen(3000);
